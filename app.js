ae70a34395c560ca63288b835c602e805442939d
Ahmed Salim <imamahmed28@gmail.com>
2022-01-12 15:48:35 +0000

    Trying to solving Cors Policy issue, using DB changes

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 const express = require('express')
+
+require('dotenv').config()
+
 const cors = require('cors')
 
 const staffRoutes = require('./routes/staffRoutes')
 
-require('dotenv').config()
+
+const PORT = process.env.PORT || 3000;
 
 //Connect to DB
 require('./database/connect')
+const connectDB = require('./database/connect');
 
 const app = express()
 
-// app.use(cors({
-//     origin:'*'
-// }))
+app.use(cors({
+    origin:'*'
+}))
 
-app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "*")
-    next()
-})
+connectDB();
+
+// app.use((req, res, next)=>{
+//     res.header("Access-Control-Allow-Origin", "*")
+//     next()
+// })
 
 app.use(cors({
     origin:'*'
@@ -35,7 +42,8 @@ app.use('/', (req, res, next)=>{
     res.status(200).json({message: 'E-Referral API is running'})
 })
 
-const PORT = process.env.NODE_ENV=="development" ? 3000 : process.env.PORT;
+
+// const PORT = process.env.NODE_ENV=="development" ? 3000 : process.env.PORT;
  
 app.listen(PORT, ()=>{
     console.log(`API Server Running PORT at ${PORT}`)
--- database/connect.js
+++ database/connect.js
@@ -1,32 +1,46 @@
 const mongoose = require("mongoose");
 
 
-let connectLocal = async () => {
-    try {
-        await mongoose.connect("mongodb://localhost:27017/e-referral-system", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("Local DB Connected");
-    } catch (e) {
-        console.log(e);
+// let connectLocal = async () => {
+//     try {
+//         await mongoose.connect("mongodb://localhost:27017/e-referral-system", {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         });
+//         console.log("Local DB Connected");
+//     } catch (e) {
+//         console.log(e);
+//     }
+// };
+
+// let connectRemote = async () => {
+//     try {
+//         await mongoose
+//             .connect(process.env.MONGO_URI, {
+//                 useNewUrlParser: true,
+//                 useUnifiedTopology: true,
+//             })
+//         console.log("Remote DB Connected");
+//     } catch (e) {
+//         console.log(e);
+//     }
+// };
+
+// if (process.env.NODE_ENV == "development") connectLocal();
+// // else connectRemote();
+
+// mongoose.Promise = global.Promise;
+
+
+// Testing
+const connectDB = async()=>{
+    try{
+        const con = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongDB connected: ${con.connection.host}`);
+    }catch(err){
+        console.log(err);
+        process.exit(1);
     }
 };
 
-let connectRemote = async () => {
-    try {
-        await mongoose
-            .connect(process.env.MONGO_URI, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            })
-        console.log("Remote DB Connected");
-    } catch (e) {
-        console.log(e);
-    }
-};
-
-if (process.env.NODE_ENV == "development") connectLocal();
-else connectRemote();
-
-mongoose.Promise = global.Promise;
+module.exports = connectDB;
