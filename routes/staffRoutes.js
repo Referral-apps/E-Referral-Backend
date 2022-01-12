5fd77e1990f5b2b3ed5d8803e5b26d0c3ade4d9a
Ahmed Salim <imamahmed28@gmail.com>
2022-01-12 12:04:20 +0000

    *

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -7,8 +7,7 @@ const superAdminAuth = require('../middleware/superAdminAuth');
 
 const staffController = require('../controllers/staffController');
 
-// router.post('/staff/register', auth, staffController.register);
-router.post('/staff/register', staffController.register);
+router.post('/staff/register', auth, staffController.register);
 
 router.post('/staff/login', staffController.login);
 
